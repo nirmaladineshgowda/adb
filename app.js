12d3206e20e096761c3e8f5724b5c2742931fed6
manojranga619 <84231573+manojranga619@users.noreply.github.com>
2022-02-13 21:42:11 -0600

    Testing DB

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 const express = require("express");
 const cors = require('cors');
+const bodyparser = require("body-parser");
+const connection = require("./connection");
+const { Request } = require("tedious");
 
 var app = express();
-app.use(cors({
-    origin: '*'
-}));
+app.use(bodyparser.json());
+app.use(cors({ origin: '*' }));
 
 // Default Route
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
+});
+
+app.post('/searchnamekeydb', (req, res) => {
+    var name = req.body.Name;
+    var value = req.body.Value;
+    var sql = `SELECT TOP 20 * FROM [details]`;
+    // Read all rows from table
+    const request = new Request(sql,
+        (err, rowCount) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log(`${rowCount} row(s) returned`);
+            }
+        }
+    );
+    const rows = [];
+    request.on("row", columns => {
+        let row = {};
+        columns.forEach((column) => {
+            row[column.metadata.colName] = column.value;
+        });
+        rows.push(row);
+    });
+
+    request.on('done', (rowCount) => {
+        console.log('Done is called!');
+        res.send(rows);
+      });
+    
+    request.on('doneInProc', (rowCount, more) => {
+        console.log(rowCount + ' rows returned');
+        res.send(rows);
+    });
+   connection.execSql(request);
 });
 
 const PORT = process.env.PORT || 3000;
