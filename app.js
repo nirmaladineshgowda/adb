d1dbe472ab100e50b43d106e27d8f7ad8174eded
manojranga619 <84231573+manojranga619@users.noreply.github.com>
2022-02-13 18:59:11 -0600

    Test Service

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require('cors');
+
+var app = express();
+app.use(cors({
+    origin: '*'
+}));
+
+// Default Route
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+const PORT =3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
