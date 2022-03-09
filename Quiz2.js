f6803ab53cd03e77d02d90a08684cb6f3a1ec0a1
manojranga619 <84231573+manojranga619@users.noreply.github.com>
2022-03-09 17:14:36 -0600

    y

--- Quiz2.js
+++ Quiz2.js
@@ -2,23 +2,17 @@
 let baseAddress = 'http://localhost:3000/';
 
 function getQuakesByMagRange() {
-    let magRange1 = document.getElementById("magRange1").value;
-    let magRange2 = document.getElementById("magRange2").value;
-    let place = document.getElementById("place").value;
-
+    let magRange1 = document.getElementById("minId").value;
+    let magRange2 = document.getElementById("ToId").value;
     
-    fetch(`${baseAddress}getQuakesByMagRange`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ magRange1: magRange1, magRange2: magRange2, place: place }),
-    })
+    fetch(`${baseAddress}getIdsRange`)
         .then(response => response.json())
         .then(data => {
             console.log('Success:', data);
-            data = data.filter(x => x.place.includes(place));
-            displayQuakes(data);
+            const time = data.time;
+            data = data.data.filter(x => x.id >= Number(magRange1) && x.id <= Number(magRange2));
+            document.getElementById("time").innerText = time;
+            displayQuakes(data, time);
         })
         .catch((error) => {
             console.error('Error:', error);
@@ -31,7 +25,7 @@ function displayQuakes(data) {
     for (var i = 0; i < data.length; i++) {
         var div = document.createElement("div");
         div.style.padding = '20px';
-        div.innerHTML = "Calamity ID: " + data[i].id + "      Magnitude: " + data[i].mag + "     Type:" + data[i].type;
+        div.innerHTML = "ID: " + data[i].id + "      Name: " + data[i].name + "     pwd:" + data[i].pwd + "   code:" + data[i].code;
         mainContainer.appendChild(div);
     }
 }
